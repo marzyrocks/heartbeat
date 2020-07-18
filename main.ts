e5f6d24d44afb758cd1ece6aecc61ca988e453da
marzyrocks <66854721+marzyrocks@users.noreply.github.com>
2020-07-17 17:11:27 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+})
